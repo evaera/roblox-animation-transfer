f2e9766d8597f5b4728e2101ed898daf0bce8787
Eryn Lynn <eryn@eryn.io>
2019-02-01 01:02:01 -0500

    Fix #1

--- src/animation.ts
+++ src/animation.ts
@@ -1,28 +1,26 @@
 import * as endpoints from './endpoints'
-import axios from 'axios'
+import fetch from 'node-fetch'
 import { State } from './state'
 
-export async function pullAnimation (id: number): Promise<string> {
-  return (await axios.get(endpoints.asset(id)).catch(() => {
+export async function pullAnimation (id: number): Promise<Buffer> {
+  return (await fetch(endpoints.asset(id)).catch(() => {
     throw new Error(`Cannot pull animation ${id}`)
-  })).data
+  })).blob()
 }
 
-export async function publishAnimation (state: State, title: string, description: string, data: string, groupId?: number): Promise<string> {
-  return (await axios.post(
+export async function publishAnimation (state: State, title: string, description: string, data: Buffer, groupId?: number): Promise<string> {
+  return (await fetch(
     endpoints.publish(title, description, groupId),
-    data,
     {
+      body: data,
+      method: 'POST',
       headers: {
         ...state.headers,
         'User-Agent': 'RobloxStudio/WinInet',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-        'Content-Type': 'application/octet-stream' // important on big animations
-      },
-      responseType: 'text'
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'
+      }
     }).catch((e) => {
       throw new Error(`Publish of animation "${title}" failed: ${e.toString()}`)
     })
-  ).data
+  ).text()
 }
