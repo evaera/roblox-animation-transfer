7db0496bff21211f2342be13310bc61b3058bf2d
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-10-23 19:06:41 -0400

    Change list endpoints to item configuration api

--- src/endpoints.ts
+++ src/endpoints.ts
@@ -1,13 +1,18 @@
-export const LIST = "https://www.roblox.com/build/assets?assetTypeId=24"
+export const LIST = "https://itemconfiguration.roblox.com/v1/creations/get-assets?assetType=24"
 export const LOGOUT = "https://auth.roblox.com/v2/logout"
 export const AUTHENTICATED = "https://users.roblox.com/v1/users/authenticated"
 
 export const asset = (id: number) =>
   `https://assetdelivery.roblox.com/v1/asset/?id=${id}`
-export const userList = (userId: number, startRow = 0) =>
-  `${LIST}&userId=${userId}&startRow=${startRow}`
-export const groupList = (groupId: number, startRow = 0) =>
-  `${LIST}&groupId=${groupId}&startRow=${startRow}`
+export const userList = (userId: number, cursor?: string) =>
+  LIST +
+  "&limit=100" +
+  (cursor != null ? `&cursor=${cursor}` : "")
+export const groupList = (groupId: number, cursor?: string) =>
+  LIST +
+  `${groupId}` +
+  "&limit = 100" +
+  (cursor != null ? `cursor=${cursor}` : "")
 export const publish = (title: string, description: string, groupId?: number) =>
   "https://www.roblox.com/ide/publish/uploadnewanimation" +
   "?assetTypeName=Animation" +
