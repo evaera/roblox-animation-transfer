571c390381314ff91dc3d3d1d14d079e8eae2b54
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2023-02-18 01:12:28 -0500

    Read Roblox Studio's binarycookies file on macOS

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,13 @@
 import chalk from "chalk"
 import * as fs from "fs"
 import { findPassword } from "keytar";
+import { promisify } from "util"
 import argv from "./argv"
 import { getGroupList, getUserList } from "./list"
 import getState from "./state"
 import transfer from "./transfer"
+
+const readFile = promisify(fs.readFile)
 
 const fatal = (errorText: string) => {
   console.error(chalk.bold.red(errorText))
@@ -20,8 +23,29 @@ const assert = (condition: any, errorText: string) => {
 }
 
 async function getCookieFromRobloxStudio(): Promise<undefined | string> {
-  if (process.platform !== "win32") {
+  if (["darwin", "win32"].includes(process.platform)) {
     return
+  }
+
+  if (process.platform === "darwin") {
+    try {
+      const binaryCookieData = await readFile(
+        `~/Library/HTTPStorages/com.Roblox.RobloxStudio.binarycookies`,
+        { encoding: "utf-8" }
+      )
+
+      const matchGroups = binaryCookieData.match(
+        /\.ROBLOSECURITY\/RobloxStudioAuth(_\|WARNING:-DO-NOT-SHARE-THIS\.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items\.\|_[A-F\d]+)/
+      )
+
+      if (!matchGroups || matchGroups.length < 2) {
+        return
+      }
+
+      return matchGroups[1]
+    } catch {
+      return
+    }
   }
 
   const cookie = await findPassword("https://www.roblox.com:RobloxStudioAuth.ROBLOSECURITY")
